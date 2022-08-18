05579d21c622146c144e49e73d586644f84e0520
Collins <mussocollins@gmail.com>
2022-08-18 17:59:04 +0300

    Add Quote link

---
+++ src/pages/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Quote.css';
+
+function Quote() {
+  return (
+    <div className="math-quotes">
+      <p>
+        Pure mathematics is, in its way, the poetry of logical ideas.
+        <p />
+        <p>Albert Einstein, German theoretical physicist.</p>
+      </p>
+    </div>
+  );
+}
+
+export default Quote;
