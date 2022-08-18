a98a5314b3e9c644274d79f85a8b4c9f16c54ce9
Collins <mussocollins@gmail.com>
2022-08-18 19:27:49 +0300

    Add routing on pages

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
     <nav className="nav-section">
       <a className="math-heading" href="/mathHeading">Math Magicians</a>
       <ul className="nav-group">
-        <li><a href="/home">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/calculator">Calculator</a></li>
         <li><a href="/quote">Quote</a></li>
       </ul>
